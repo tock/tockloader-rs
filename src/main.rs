baf3f1d621f8ab9f65d4c1f1c33c82cec822bef1
Cosma George <georgemagic10@gmail.com>
2023-07-27 16:09:40 +0300

    switched tockloader to be a root repository

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use cli::make_cli;
 fn main() {
     let matches = make_cli().get_matches();
 
+    println!("{}", tbf_parser::add(1, 2));
+
     if matches.get_flag("debug") {
         println!("Debug mode enabled");
     }
--- tbf-parser/src/lib.rs
+++ tbf-parser/src/lib.rs
@@ -1,3 +1,17 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+
+#[cfg(feature = "std")]
+pub fn add(left: usize, right: usize) -> usize {
+    let a = [left, right].to_vec();
+    let mut sum = 1;
+    for item in a {
+        sum += item
+    }
+    return sum
+}
+
+#[cfg(not(feature = "std"))]
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
