8cfc20c7b71bf320f08d7ff675074914e86c3028
Cosma George <georgemagic10@gmail.com>
2023-08-09 00:03:45 +0300

    Added RSA4096 test

--- tbf-parser/tests/parse.rs
+++ tbf-parser/tests/parse.rs
@@ -73,3 +73,52 @@ fn footer_sha256() {
         panic!("Footer is not of type 'Reserved'!");
     }
 }
+
+#[test]
+fn footer_rsa4096() {
+    let buffer: Vec<u8> = include_bytes!("./flashes/footerRSA4096.dat").to_vec();
+
+    let (ver, header_len, whole_len) = parse_tbf_header_lengths(&buffer[0..8].try_into().unwrap())
+        .ok()
+        .unwrap();
+    assert_eq!(ver, 2);
+    assert_eq!(header_len, 76);
+    assert_eq!(whole_len, 4096);
+
+    let header = parse_tbf_header(&buffer[0..header_len as usize], 2).unwrap();
+    dbg!(&header);
+    assert_eq!(header.enabled(), true);
+    assert_eq!(header.get_minimum_app_ram_size(), 4612);
+    assert_eq!(header.get_init_function_offset(), 41 + header_len as u32);
+    assert_eq!(header.get_protected_size(), header_len as u32);
+    assert_eq!(header.get_package_name().unwrap(), "c_hello");
+    assert_eq!(header.get_kernel_version().unwrap(), (2, 0));
+    let binary_offset = header.get_binary_end() as usize;
+    assert_eq!(binary_offset, 1168);
+
+    let (footer, footer_size) = parse_tbf_footer(&buffer[binary_offset..]).unwrap();
+    dbg!(footer);
+    assert_eq!(footer_size, 1028);
+    let correct_key = include_bytes!("./flashes/RSA4096.key");
+    let correct_signature = include_bytes!("./flashes/RSA4096.sig");
+    if let TbfFooterV2Credentials::Rsa4096Key(creds) = footer {
+        assert_eq!(
+            creds.get_format().unwrap(),
+            TbfFooterV2CredentialsType::Rsa4096Key
+        );
+        assert_eq!(creds.get_public_key(), correct_key);
+        assert_eq!(creds.get_signature(), correct_signature);
+    } else {
+        panic!("Footer is not of type SHA256!");
+    }
+
+    let second_footer_offset = binary_offset + footer_size as usize + 4;
+    let (footer, footer_size) = parse_tbf_footer(&buffer[second_footer_offset..]).unwrap();
+    dbg!(footer);
+    assert_eq!(footer_size, 1892);
+    if let TbfFooterV2Credentials::Reserved(padding) = footer {
+        assert_eq!(padding, 1892);
+    } else {
+        panic!("Footer is not of type 'Reserved'!");
+    }
+}
