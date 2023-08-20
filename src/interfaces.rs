40c9c6330ea1fc46aa2589519f5fce6cacd4ccd9
Cosma George <georgemagic10@gmail.com>
2023-08-20 14:00:48 +0300

    Added enum dispatch

--- src/interfaces.rs
+++ src/interfaces.rs
@@ -1,4 +1,18 @@
+use self::{
+    board_interface::BoardInterface, jlink::JLinkInterface, openocd::OpenOCDInterface,
+    serial::SerialInterface,
+};
+use crate::errors::TockloaderError;
+use enum_dispatch::enum_dispatch;
+
 pub mod board_interface;
 pub mod jlink;
 pub mod openocd;
 pub mod serial;
+
+#[enum_dispatch(BoardInterface)]
+pub enum Interface {
+    Serial(SerialInterface),
+    OpenOCD(OpenOCDInterface),
+    JLink(JLinkInterface),
+}
--- src/interfaces/board_interface.rs
+++ src/interfaces/board_interface.rs
@@ -1,5 +1,8 @@
 use crate::errors::TockloaderError;
 
+use enum_dispatch::enum_dispatch;
+
+#[enum_dispatch]
 pub trait BoardInterface {
     fn open(&mut self) -> Result<(), TockloaderError>;
 }
